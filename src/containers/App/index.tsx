95a003508aac197475d38180052468a1317d77cc
Shriharsha KL <shriharsha.kl@clumio.com>
2020-01-07 20:55:31 +0530

    Add mock todos and render todos list

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -3,14 +3,23 @@ import React, {useState} from 'react';
 import Input, {Todo} from "../../components/Input";
 import TodosList from "../../components/TodosList";
 
+const mock: Todo[] = [{
+    value: 'Todo item 1',
+    completed: false,
+}, {
+    value: 'Todo item 2',
+    completed: false,
+}, {
+    value: 'Todo item 3',
+    completed: false,
+}]
+
 const Index: React.FC = () => {
-    const [todos, setTodos] = useState<Todo[]>([]);
+    const [todos, setTodos] = useState<Todo[]>(mock);
 
     const updateTodos = (t: Todo) => {
         setTodos([t]);
     };
-
-    console.log(todos)
 
     return (
         <div className="container">
