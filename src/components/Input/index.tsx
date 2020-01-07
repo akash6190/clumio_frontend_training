e9f6e82cec2b77167fceb900f5eb8cb0f73f5f9c
Shriharsha KL <shriharsha.kl@clumio.com>
2020-01-07 19:52:08 +0530

    Wire up controlled input, set up todos state

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, {ChangeEventHandler, FormEventHandler, useState} from 'react';
+
+interface Todo {
+    value: string;
+    completed: boolean;
+}
 
 const Input: React.FC = () => {
-    return <input type="text" className="input" placeholder="What needs to be done?" />
+    const [value, setValue] = useState('');
+    const [todos, setTodos] = useState<Todo[]>([]);
+
+    const handleChange: ChangeEventHandler<HTMLInputElement> = e => {
+        setValue(e.target.value);
+    };
+
+    const handleFormSubmit: FormEventHandler<HTMLFormElement> = e => {
+        e.preventDefault();
+        setTodos([{ completed: false, value: value.trim() }]);
+        setValue('');
+    };
+
+    console.log(todos);
+
+    return (
+        <form onSubmit={handleFormSubmit}>
+            <input
+                type="text"
+                className="input"
+                placeholder="What needs to be done?"
+                value={value}
+                onChange={handleChange}
+            />
+        </form>
+    );
 };
 
 export default Input;
