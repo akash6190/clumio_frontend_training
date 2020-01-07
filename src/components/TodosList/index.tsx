eb0eb0b6157a218c7d753799f46d29177fc0bebf
Shriharsha KL <shriharsha.kl@clumio.com>
2020-01-07 22:25:53 +0530

    Implement todo completed logic

--- src/components/TodosList/index.tsx
+++ src/components/TodosList/index.tsx
@@ -1,18 +1,25 @@
 import React from "react";
+
 import {Todo} from "../Input";
+import Check from '../../check.svg';
 
 interface Props {
-    todos: Todo[]
+    todos: Todo[];
+    onComplete: (t: Todo) => void;
 }
 
-const TodosList: React.FC<Props> = ({ todos }) => {
+const TodosList: React.FC<Props> = ({ onComplete, todos }) => {
     return todos ? (
         <ul className="list-container">
             {
-                todos.map(({ value, completed }) => (
+                todos.map(todo => (
                     <li className="list-item">
-                        <div className="check-circle"></div>
-                        <span>{value}</span>
+                        <div className="check-circle" onClick={() => onComplete(todo)}>
+                            {
+                                todo.completed ? <img src={Check} /> : null
+                            }
+                        </div>
+                        <span>{todo.value}</span>
                     </li>
                 ))
             }
